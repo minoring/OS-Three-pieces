3f40953564141a7856944fbf13779ceeee6cf6e4
minoring <minho1350@gmail.com>
2019-03-24 19:50:02 +0900

    intro

---
+++ intro/cpu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+#include "common.h"
+
+int main(int argc, char const *argv[]) {
+        if (argc != 2) {
+                fprintf(stderr, "usage: cpu <string>\n");
+                exit(1);
+        }
+        const char* str = argv[1];
+        while (1) {
+                Spin(1);
+                printf("%s\n", str);
+        }
+        return 0;
+}
+
+// ./cpu A & ; ./cpu B & ; ./cpu C & ; ./cpu D &
+// Explain virtual CPU
